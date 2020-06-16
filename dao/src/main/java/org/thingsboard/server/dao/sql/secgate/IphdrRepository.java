8b5dc573246287fd3902bdbcf3233a120ca3a048
xuchuanjia <1324139335@sia.cn>
2020-06-16 15:09:11 +0800

    2020/6/16 增加iphdr表的entity与repository，增加IDSController及所需bean类

--- dao/src/main/java/org/thingsboard/server/dao/sql/secgate/IphdrRepository.java
+++ dao/src/main/java/org/thingsboard/server/dao/sql/secgate/IphdrRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.thingsboard.server.dao.model.sql.IphdrEntity;
 
 public interface IphdrRepository extends JpaRepository<IphdrEntity, Integer> {
+
+
 }

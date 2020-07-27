7755c4ad768e9339b00ede16551284ca62d79508
Chen87 <ccy9287@163.com>
2020-07-27 16:38:36 +0800

    package

--- dao/src/main/resources/sql/system-data.sql
+++ dao/src/main/resources/sql/system-data.sql
@@ -18,8 +18,8 @@
 
 /** System admin **/
 INSERT INTO tb_user ( id, tenant_id, customer_id, email, search_text, authority )
-VALUES ( '1e746125a797660a91992ebcb67fe33', '1b21dd2138140008080808080808080', '1b21dd2138140008080808080808080', 'sysadmin@thingsboard.org',
-         'sysadmin@thingsboard.org', 'SYS_ADMIN' );
+VALUES ( '1e746125a797660a91992ebcb67fe33', '1b21dd2138140008080808080808080', '1b21dd2138140008080808080808080', 'sysadmin@sia.org',
+         'sysadmin@sia.org', 'SYS_ADMIN' );
 
 INSERT INTO user_credentials ( id, user_id, enabled, password )
 VALUES ( '1e7461261441950a91992ebcb67fe33', '1e746125a797660a91992ebcb67fe33', true,
@@ -33,7 +33,7 @@ VALUES ( '1e746126a2266e4a91992ebcb67fe33', 'general', '{
 
 INSERT INTO admin_settings ( id, key, json_value )
 VALUES ( '1e746126eaaefa6a91992ebcb67fe33', 'mail', '{
-	"mailFrom": "Thingsboard <sysadmin@localhost.localdomain>",
+	"mailFrom": "SIASEC <sysadmin@localhost.localdomain>",
 	"smtpProtocol": "smtp",
 	"smtpHost": "localhost",
 	"smtpPort": "25",
